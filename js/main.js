015a4ccab6bdad475c2e5fbb404782142e497e82
Steven Ried <70533094+stevenried@users.noreply.github.com>
2022-08-05 13:09:29 -0400

    Add code to fetch iss api data and draw map using leaflet.js library

---
+++ js/main.js
@@ -0,0 +1,31 @@
+//Set endpoint
+const apiEndpoint = 'https://api.wheretheiss.at/v1/satellites/25544'
+
+//Making map and tiles
+const map = L.map('issMap').setView([0.0, 0.0], 1)
+const tileURL = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+const tiles = L.tileLayer(tileURL, {
+  maxZoom: 19,
+  attribution: '© OpenStreetMap',
+}).addTo(map)
+
+//Making a marker with custom icon
+const issIcon = L.icon({
+  iconUrl: './assets/iss.png',
+  iconSize: [50, 32],
+  iconAnchor: [25, 16],
+})
+const marker = L.marker([0.0, 0.0], { icon: issIcon }).addTo(map)
+
+getISS()
+
+async function getISS() {
+  const response = await fetch(apiEndpoint)
+  const data = await response.json()
+  const { latitude, longitude } = data
+
+  marker.setLatLng([latitude, longitude])
+
+  document.querySelector('[data-id-lat]').textContent = latitude
+  document.querySelector('[data-id-long]').textContent = longitude
+}
